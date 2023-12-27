612cfb6bfeca18279fc339d6c09e0c98b18a8e91
Xiyu Pan <jk236777@gmail.com>
2023-12-27 18:41:20 +0800

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer">Xiyu Pan 2023</div>;
+  return <div className="footer">Wiily Pan 2023</div>;
 };
 
 export default Footer;
